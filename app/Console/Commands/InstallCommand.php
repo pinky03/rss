844290d0c0e55d5f12f7498b251090e0586101a6
pinky03 <alextsdlv@gmail.com>
2021-07-03 14:21:19 +0300

    add app:install command

---
+++ app/Console/Commands/InstallCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install\reset application';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->call('migrate:fresh');
+
+        $this->call('orchid:admin', [
+            'name' => 'admin',
+            'email' => env('APP_ADMIN_MAIL', 'admin@admin.com'),
+            'password' => env('APP_ADMIN_PASSWORD', 'admin'),
+        ]);
+
+        return 0;
+    }
+}
